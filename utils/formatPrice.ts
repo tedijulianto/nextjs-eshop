86270fb0031f43e6a4b1c22d866ddd63c2bf32fd
tedijulianto <tedijulianto92@gmail.com>
2023-11-18 11:32:30 +0700

    remove decimal format price

--- utils/formatPrice.ts
+++ utils/formatPrice.ts
@@ -2,5 +2,6 @@ export const formatPrice = (amount: number) => {
   return new Intl.NumberFormat("id-ID", {
     currency: "IDR",
     style: "currency",
+    minimumFractionDigits: 0,
   }).format(amount);
 };
